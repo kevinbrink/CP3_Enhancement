0ace258421f34e5be9a72ea4035b4194e419ae25
Gabe Martin <funkyman1122@gmail.com>
2014-09-24 13:40:46 -0400

    Custom Poll Class
    Added a new custom poll class and also modified the CreateSlide method
    to read in poll data from a PPT file.

--- UW.ClassroomPresenter/Decks/PPTDeckIO.cs
+++ UW.ClassroomPresenter/Decks/PPTDeckIO.cs
@@ -19,6 +19,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Office.Core;
 
 namespace UW.ClassroomPresenter.Decks {
 
@@ -186,6 +187,30 @@ namespace UW.ClassroomPresenter.Decks {
             PowerPoint.Shapes currentShapes = currentSlide.Shapes;
 
             List<TaggedShape> taggedShapeList = PPTDeckIO.BuildTaggedShapeList(currentShapes, pptpm);
+
+            /* The following code was added by Gabriel Martin on Sept 24, 2014 */
+
+            /* Check to see if there is any custom XML in the slide */
+            if (currentSlide.CustomerData.Count != 0)
+            {
+                /* Grab the custom XML for a poll */
+                CustomXMLPart pollXML = (CustomXMLPart)currentSlide.CustomerData._Index(1);
+
+                /* Set answrs to null */
+                CustomXMLNodes answrs = null;
+
+                /* If the poll type is not "true or false" then set answrs to the child nodes of /CP3Poll/PollAnswers */
+                if (!pollXML.SelectSingleNode("/CP3Poll/PollType").Text.Equals("True or False"))
+                {
+                    answrs = pollXML.SelectSingleNode("/CP3Poll/PollAnswers").ChildNodes;
+                }
+
+                /* Create a new poll */
+                CustomPoll poll = new CustomPoll(pollXML.SelectSingleNode("/CP3Poll/PollQuestion").Text, pollXML.SelectSingleNode("/CP3Poll/PollType").Text, Convert.ToInt32(pollXML.SelectSingleNode("/CP3Poll/PollSlide").Text),
+                    pollXML.SelectSingleNode("/CP3Poll/PollCorrectAnswer").Text, answrs);
+            }
+
+            /* End of modification */
 
             //Create a new SlideModel
             SlideModel newSlideModel = new SlideModel(Guid.NewGuid(), new LocalId(), SlideDisposition.Empty, new Rectangle(0, 0, slideWidth, slideHeight));
